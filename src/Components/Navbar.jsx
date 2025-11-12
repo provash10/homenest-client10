b9b93b7e2bb398cb1d6f1bbdfc808bc7083f2142
Provash <provash20cb@gmail.com>
2025-11-12 17:21:13 +0800

    User with login & logout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,53 +1,58 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router';
 import { AuthContext } from '../Contexts/AuthContext';
+import toast from 'react-hot-toast';
 
 const Navbar = () => {
- const { user, signOutUserFunc, setUser } = useContext(AuthContext) || {};
-  
- const handleLogout = () => {
+  const { user, signOutUserFunc, setUser } = useContext(AuthContext) || {};
+
+  const handleLogout = () => {
     signOutUserFunc()
       .then(() => {
+        toast.success("Logout Successfull")
         setUser(null);
       })
       .catch((error) => {
         console.error(error);
+        toast.error(error.message);
       });
   };
 
-  const links =(
+  const links = (
     <>
-    <li><NavLink to='/'>Home</NavLink></li>
-    <li><NavLink to='/all-propertise'>All Propertise</NavLink></li>
-    <li><NavLink to='/add-propertise'>Add Propertise</NavLink></li>
-    <li><NavLink to='/my-propertise'>My Propertise</NavLink></li>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/all-propertise'>All Propertise</NavLink></li>
+      <li><NavLink to='/add-propertise'>Add Propertise</NavLink></li>
+      <li><NavLink to='/my-propertise'>My Propertise</NavLink></li>
     </>
   )
-    return (
-        <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        {links}
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">Home Nest 10</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      {links}
-    </ul>
-  </div>
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
 
-  <div className="navbar-end gap-2">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Home Nest 10</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {links}
+        </ul>
+      </div>
+
+      <div className="navbar-end gap-2">
         {user ? (
           <>
+            <img src={user?.photoURL || "https://via.placeholder.com/88"} 
+            className='border h-20 w-20 rounded-full mx-auto' alt="" />
             <span className="font-semibold">{user.displayName || "User"}</span>
             <button
               onClick={handleLogout}
@@ -66,8 +71,8 @@ const Navbar = () => {
         )}
       </div>
 
-</div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
